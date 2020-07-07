53160bb950fdf9bfabbd0b074702c0892be6bb76
aishvarya779 <aishvaryatiwari123@gmail.com>
2020-07-07 17:22:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+let colors = [
+  { key: 1, color: '#6F98A8' },
+  { key: 2, color: '#2B8EAD' },
+  { key: 3, color: '#2F454E' },
+  { key: 4, color: '#2B8EAD' },
+  { key: 5, color: '#2F454E' },
+  { key: 6, color: '#BFBFBF' },
+  { key: 7, color: '#BFBFBF' },
+  { key: 8, color: '#6F98A8' },
+  { key: 9, color: '#2F454E' }
+];
+
+let selectedItem = document.getElementById('main');
+const appendCards = arr => {
+  selectedItem.innerHTML = '';
+  let result = '';
+  arr.forEach(item => {
+    let node = document.createElement('div');
+    node.className = 'box';
+    node.style.backgroundColor = item.color;
+    var textnode = document.createTextNode(item.key);
+    node.appendChild(textnode);
+    selectedItem.appendChild(node);
+  });
+  console.log(colors);
+};
+
+const shuffleCards = () => {
+  let shuffledArray = colors;
+  shuffledArray.sort(() => Math.random() - 0.5);
+  appendCards(shuffledArray);
+};
+
+const sortCards = () => {
+  let sortedArray = colors.sort((a, b) => {
+    if (a > b) return 1;
+    if (a < b) return -1;
+    return 0;
+  });
+  appendCards(sortedArray);
+};
+sortCards();
